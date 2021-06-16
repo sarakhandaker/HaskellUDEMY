b303da83bd04875b0ed93cf78ce2dc7dd42e44d1
sarakhandaker <kttsara@gmail.com>
2021-06-16 11:54:18 -0700

    quaternion

--- C08172/Fundamentals of Practical Haskell Programming [Video]/Volume_01_Section_03_Topic_04/Quaternion.hs
+++ C08172/Fundamentals of Practical Haskell Programming [Video]/Volume_01_Section_03_Topic_04/Quaternion.hs
@@ -1,6 +1,6 @@
 ------------------------------------------------------------
 --
--- Quaternion3.hs
+-- Quaternion1.hs
 -- Code sample accompanying topic 1.3.4 "Type classes"
 -- See README.md for details
 --
@@ -14,7 +14,23 @@ data Quaternion = Q
     , qI :: Double
     , qJ :: Double
     , qK :: Double
-    } deriving Show
+    }
+
+-- without this, Quaternion doesnt have a show instance
+instance Show Quaternion where
+    show q = "(" ++
+        show (qR q) ++ " + " ++
+        show (qI q) ++ "i + " ++
+        show (qJ q) ++ "j + " ++
+        show (qK q) ++ "k)"
+
+-- this is so common in Haskell you can just do the following:
+-- data Quaternion = Q
+--     { qR :: Double
+--     , qI :: Double
+--     , qJ :: Double
+--     , qK :: Double
+--     } deriving Show
 
 instance Num Quaternion where
     q0 + q1 = Q (qR q0 + qR q1) (qI q0 + qI q1) (qJ q0 + qJ q1) (qK q0 + qK q1)
@@ -25,4 +41,5 @@ instance Num Quaternion where
     negate = undefined
 
 main :: IO ()
-main = print $ Q 10 20 30 40 + Q 100 200 300 400
+main = print $ Q 1 2 3 4
+        print $ Q 10 20 30 40 + Q 100 200 300 400
