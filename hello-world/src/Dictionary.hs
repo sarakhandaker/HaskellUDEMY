c18ad7d7d81a4da56fc67637e01d402f6f0e8d6f
sarakhandaker <kttsara@gmail.com>
2021-06-22 13:10:46 -0700

    functions and types

--- hello-world/src/Dictionary.hs
+++ hello-world/src/Dictionary.hs
@@ -1,3 +1,22 @@
+data Compass = North | South | East | West
+    deriving (Eq, Show, Ord, Enum)
+
+data Expresson = Number Int
+                | Add Expresson Expresson
+                | Subtract Expresson Expresson
+    deriving (Eq, Show, Ord)
 
 main = print $ lookup "one" dict
     where dict = [("one", 1), ("two", 2), ("three", 3)]
+
+    -- maybe return value will return a number (with just) or nothing
+    -- use stack ghc -- -dynamic test.hs to make exe file smaller?
+    -- partial function: has outputs for SOME inputs (ex. either an integer or error)
+
+
+    --Partially Applied function
+
+    -- add1 = add 1
+    -- add1 b = add 1 b
+
+    -- product vs sum types (product tyes take parameters in constructor)
