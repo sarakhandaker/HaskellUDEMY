e7c2a9eaeeee8417c86808ef024aa139baa016ba
sarakhandaker <kttsara@gmail.com>
2021-07-13 11:19:20 -0700

    added unit tests

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -1,8 +1,6 @@
 module Lib
     ( formatGrid
     , outputGrid
-    , languages
-    , grid
     , skew
     , findWord
     , findWords
@@ -50,29 +48,3 @@ findWords grid words =
 
 findWordInLine :: String -> String -> Bool
 findWordInLine = isInfixOf
-
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
--- words/test/Spec.hs
+++ words/test/Spec.hs
@@ -1,9 +1,22 @@
 import Test.Hspec
 import Lib
+import Data
 
 main :: IO ()
 main = hspec $ do
-  describe "How to write a test" $ do
-    it "should be able to run tests" $ do
-      someString `shouldBe` "someString"
-      
+   describe "formatGrid" $ do
+    it "should concatenate every line with a new line" $ do
+      formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n"
+
+    describe "findWord" $ do
+        it "should find words that exist in the grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PERL" `shouldBe` Just "PERL"
+        it "should not find words that do not exist in the grid" $ do    
+            findWord grid "HAMSTER" `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "should find all the words that exist in the grid" $ do
+            findWords grid languages `shouldBe` languages
+        it "should not find all the words that do not exist in the grid" $ do
+            findWords grid ["FRENCH", "GERMAN", "ENGLISH"] `shouldBe` []
