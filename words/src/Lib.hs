26b9237ed532155ef4f64edfaab6bd163487cdff
sarakhandaker <kttsara@gmail.com>
2021-07-14 14:43:11 -0700

    output grid with coords

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -5,20 +5,39 @@ module Lib
     , findWord
     , findWords
     , findWordInLine
+    , gridWithCoords
+    , zipOverGrid
+    , zipOverGridWith
     ) where
 
 import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
-type Grid = [String]
+type Grid a = [[a]]
+data Cell = Cell (Integer, Integer) Char | Empty deriving (Eq, Ord, Show)
 
-outputGrid :: Grid -> IO ()
+zipOverGrid :: Grid a -> Grid b -> Grid (a,b)
+zipOverGrid = zipWith zip
+
+zipOverGridWith :: (a -> b -> c) -> Grid a -> Grid b -> Grid c
+zipOverGridWith = zipWith . zipWith
+
+coordsGrid :: Grid (Integer, Integer)
+coordsGrid = 
+    let cols= repeat [0..]
+        rows = map repeat [0..]
+    in zipOverGrid rows cols
+
+gridWithCoords :: Grid Char ->  Grid Cell
+gridWithCoords grid = zipOverGridWith Cell coordsGrid grid
+
+outputGrid :: Grid Char -> IO ()
 outputGrid grid = putStrLn $ formatGrid grid
 
-formatGrid :: Grid -> String
+formatGrid :: Grid Char -> String
 formatGrid = unlines
 
-getLines :: Grid -> [String]
+getLines :: Grid Char -> [String]
 getLines grid = 
     let horizontal = grid 
         vertical = transpose grid
@@ -27,21 +46,21 @@ getLines grid =
         lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
     in lines ++ (map reverse lines)
 
-diagonalize :: Grid -> Grid
+diagonalize :: Grid Char -> Grid Char
 diagonalize = transpose . skew
 
-skew :: Grid -> Grid
+skew :: Grid Char -> Grid Char
 skew [] = []
 skew (l:ls) = l : skew (map indent ls)
     where indent line = '_' : line
 
-findWord :: Grid -> String -> Maybe String
+findWord :: Grid Char -> String -> Maybe String
 findWord grid word = 
     let lines = getLines grid
         found = or $ map (findWordInLine word) lines
     in if found then Just word else Nothing
 
--- findWords :: Grid -> [String] ->  [String]
+findWords :: Grid Char -> [String] -> [String]
 findWords grid words = 
     let foundWords = map (findWord grid) words
     in catMaybes foundWords
--- words/src/Test.hs
+++ words/src/Test.hs
@@ -1,5 +1,7 @@
 import Control.Monad (guard)
 
+data Cell = Cell (Integer, Integer) Char deriving (Eg, Ord, Show)
+
 -- sample coordinate grid
 coords = [ [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7)]
          , [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7)]
